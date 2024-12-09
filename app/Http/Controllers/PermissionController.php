c346ae6c739c1a6649b933b149bd9fe4b23092b0
camilomaximus005 <jbermudezmu@unal.edu.co>
2024-12-09 02:46:41 -0500

    feat: 43-permission controller

---
+++ app/Http/Controllers/PermissionController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Spatie\Permission\Models\Permission;
+
+class PermissionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $permissions = Permission::paginate(10);
+        return view('permissions.index', compact('permissions'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('admin.permissions.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $permission = Permission::create($request->all());
+        $permission->save();
+        return redirect()->route('admin.permissions.index');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $permission = Permission::find($id);
+        return view('admin.permissions.show', compact('permission'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        $permission = Permission::find($id);
+        return view('admin.permissions.edit', compact('permission'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $permission = Permission::find($id);
+        $permission->update($request->all());
+        return redirect()->route('admin.permissions.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $permission = Permission::find($id);
+        $permission->delete();
+        return redirect()->route('admin.permissions.index');
+    }
+}
