99c80ef644672987ab0962dcb545cb75cdd41cf3
camilomaximus005 <jbermudezmu@unal.edu.co>
2024-12-09 02:21:42 -0500

    feat: 40-user and role controller

---
+++ app/Http/Controllers/RoleController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Spatie\Permission\Models\Role;
+
+class RoleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $roles = Role::paginate(10);
+        return view('admin.roles.index', compact('roles'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('admin.roles.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $role = Role::create($request->all());
+        return redirect()->route('admin.role.index');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $role = Role::find($id);
+        return view('admin.roles.show', compact('role'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        $role = Role::find($id);
+        return view('admin.roles.edit', compact('role'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $role = Role::find($id);
+        $role->update($request->all());
+        return redirect()->route('admin.roles.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $role = Role::find($id);
+        $role->delete();
+        return redirect()->route('admin.roles.index');
+    }
+}
